a1e657509f5d85d26fdf89e23eb8550f187892c1
therbert448 <75484057+therbert448@users.noreply.github.com>
2022-12-01 11:39:59 +0000

    Add files via upload

--- 2022/Day 01/Day1.py
+++ 2022/Day 01/Day1.py
@@ -6,6 +6,6 @@ Advent of Code
 """
 
 with open("Day1inputs.txt") as file:
-    elves = sorted([sum(map(int, l.strip().split())) for l in file.read().split("\n\n")])
+    elves = sorted(sum(map(int, l.split())) for l in file.read().split("\n\n"))
 print(f"Part One = {max(elves)}")
 print(f"Part Two = {sum(elves[-3:])}")
