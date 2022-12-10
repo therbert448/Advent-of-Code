f8cf731449b66b7667c9aa84f325f0433fa70a70
therbert448 <75484057+therbert448@users.noreply.github.com>
2022-12-10 14:04:43 +0000

    Add files via upload

---
+++ 2022/Day 10/Day10.py
@@ -0,0 +1,56 @@
+"""
+Advent of Code
+2022 Day 10
+
+@author: Tom Herbert
+"""
+
+day = 10
+
+def open_file():
+    filename = "Day" + str(day) + "inputs.txt"
+    with open(filename) as file:
+        inputs = [line.strip().split(" ") for line in file.readlines()]
+    return inputs
+
+def print_grid():
+    w, h = 40, 6
+    stringOut = ""
+    for y in range(h):
+        line = ""
+        for x in range(w):
+            if (x, y) in grid:
+                line += "#"
+            else:
+                line += " "
+        line += "\n"
+        stringOut += line
+    print(stringOut)
+
+def CRT():
+    global grid
+    strength, cycle, x = 0, 0, 1
+    grid = set()
+    for line in inputs:
+        coords = (cycle % 40, cycle//40)
+        if cycle % 40 in (x-1, x, x+1):
+            grid.add(coords)
+        cycle += 1
+        if cycle % 40 == 20:
+            strength += cycle * x
+        if len(line) == 2:
+            coords = (cycle % 40, cycle//40)
+            if cycle % 40 in (x-1, x, x+1):
+                grid.add(coords)
+            cycle += 1
+            if cycle % 40 == 20:
+                strength += cycle * x
+            val = int(line[1])
+            x += val
+    print(f"Part One = {strength}")
+    print("Part Two =")
+    print_grid()
+
+inputs = open_file()
+
+CRT()
