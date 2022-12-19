243bdcd0fcf234019ad6d52ee19a338e752a9752
therbert448 <75484057+therbert448@users.noreply.github.com>
2022-12-19 20:38:48 +0000

    Add files via upload

--- 2022/Day 19/Day19.py
+++ 2022/Day 19/Day19.py
@@ -3,9 +3,15 @@ Advent of Code
 2022 Day 19
 
 @author: Tom Herbert
-"""
 
-import time
+First solution took about 2 mins for Part One and 3 mins for Part Two, but this
+version can complete both within 20 seconds now.
+If no robot was built at time t, when it was possible to build a robot, then
+the robots that were skipped will not be built at time t+1.
+The only reason to not build a robot when possible is when you're waiting for
+enough resources to build a different type robot.
+There will never be a benefit to waiting an extra minute to build a robot.
+"""
 
 day = 19
 
@@ -26,45 +32,52 @@ def format_data():
         blueprints.append([oreCost, clayCost, obsidianCost, geodeCost])
 
 def make_robots(state, blueprint, time):
-    newStates = {tuple(state)}
-    if all(state[i] >= blueprint[-1][i] for i in range(3)):
-        if all(state[j+4] >= blueprint[-1][j] for j in range(3)):
-            newState = list(state)
-            newState[-1] += 1
-            for k, ore in enumerate(blueprint[-1]):
-                newState[k+4] -= ore
+    bots, ore, skipped = state[:4], state[4:8], list(state[8:])
+    newStates = set()
+    if all(bots[i] >= blueprint[-1][i] for i in range(3)):
+        if all(ore[j] >= blueprint[-1][j] for j in range(3)):
+            newState = list([*bots, *ore, 0, 0, 0, 0])
+            newState[-5] += 1
+            for k, cost in enumerate(blueprint[-1]):
+                newState[k+4] -= cost
             newStates = {tuple(newState)}
+        else:
+            newStates = {tuple([*bots, *ore, 0, 0, 0, 0])}
         return newStates
     for i, robot in enumerate(blueprint):
-        if i < 3 and state[i] >= max(blue[i] for blue in blueprint):
+        if skipped[i]:
             continue
-        if i < 3 and state[i+4] >= max(blue[i] for blue in blueprint) * time:
+        if i < 3 and bots[i] >= max(blue[i] for blue in blueprint):
             continue
-        if all(state[j+4] >= robot[j] for j in range(len(robot))):
-            newState = list(state)
+        if i < 3 and ore[i] >= max(blue[i] for blue in blueprint) * time:
+            continue
+        if all(ore[j] >= robot[j] for j in range(len(robot))):
+            skipped[i] = 1
+            newState = list([*bots, *ore, 0, 0, 0, 0])
             newState[i] += 1
-            for k, ore in enumerate(robot):
-                newState[k+4] -= ore
+            for k, cost in enumerate(robot):
+                newState[k+4] -= cost
             newStates.add(tuple(newState))
+    newStates.add(tuple([*bots, *ore, *skipped]))
     return newStates
 
 def pass_time(current, t, bp, minMaxGeodes = 0):
-    #print(t, len(current))
     newCurrent = set()
     for state in current:
         newStates = make_robots(state, bp, t)
         for newState in newStates:
-            nBots, nOre = tuple(newState[:4]), list(newState[4:])
+            nBots, nOre = tuple(newState[:4]), list(newState[4:8])
+            skipped = list(newState[8:])
             for i, numRobots in enumerate(state[:4]):
                 nOre[i] += numRobots
             minMaxGeodes = max(minMaxGeodes, nOre[-1] + (nBots[-1] * (t-1)))
             maxPossible = nOre[-1] + (nBots[-1] * (t-1)) + ((t-2)*(t-1))//2
             if maxPossible < minMaxGeodes:
                 continue
-            newCurrent.add(tuple([*nBots, *nOre]))
+            newCurrent.add(tuple([*nBots, *nOre, *skipped]))
             robots[nBots] = nOre[-1]
     current = set()
-    if any(state[-1] > 0 for state in newCurrent):
+    if any(state[-5] > 0 for state in newCurrent):
         for state in newCurrent:
             if not state[1] or not state[2]:
                 continue
@@ -84,9 +97,10 @@ def part_one():
     for i, blueprint in enumerate(blueprints):
         bots = (1, 0, 0, 0)
         ore = (0, 0, 0, 0)
+        skipped = (0, 0, 0, 0)
         robots = {tuple(bots): ore[-1]}
         time = 24
-        state = tuple([*bots, *ore])
+        state = tuple([*bots, *ore, *skipped])
         current = {state}
         pass_time(current, time, blueprint)
         geodes = best[-1]
@@ -100,9 +114,10 @@ def part_two():
     for i, blueprint in enumerate(blueprints[:3]):
         bots = (1, 0, 0, 0)
         ore = (0, 0, 0, 0)
+        skipped = (0, 0, 0, 0)
         robots = {tuple(bots): ore[-1]}
         time = 32
-        state = tuple([*bots, *ore])
+        state = tuple([*bots, *ore, *skipped])
         current = {state}
         pass_time(current, time, blueprint)
         geodes = best[-1]
@@ -112,11 +127,6 @@ def part_two():
 inputs = open_file()
 
 format_data()
-startp1 = time.perf_counter()
-#part_one()
-startp2 = time.perf_counter()
-part_two()
-end = time.perf_counter()
 
-print(f"Part One took {round(startp2 - startp1, 3)} seconds")
-print(f"Part Two took {round(end - startp2, 3)} seconds")
+part_one()
+part_two()
